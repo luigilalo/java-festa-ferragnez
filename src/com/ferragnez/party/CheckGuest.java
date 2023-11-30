3a0f82c080fa1ae4092e51e242317015905a9124
luigilalo <luigilaloe1@gmail.com>
2023-11-30 15:43:29 +0100

    Done with For

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,30 @@
+package com.ferragnez.party;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // CREO L'ARRAY
+        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        // RENDO VISIBILE IN CONSOLE L'ARRAY
+        System.out.println(Arrays.toString(guests));
+        // CHIEDO IL NOME ALL'UTENTE
+        System.out.println("Qual è il tuo nome?");
+        String userName = scanner.nextLine();
+        boolean userFound = false;
+        // VERIFICO SE IL NOME UTENTE E' NELLA LISTA CON IL FOR
+        for (int i = 0; i < guests.length; i++) {
+            if (userName.equals(guests[i])) {
+                userFound = true;
+                break;
+            }
+        }
+        if (userFound == true)
+            System.out.println("Puoi entrare");
+        else {
+            System.out.println("Non puoi entrare");
+        }
+    }
+}
